28962c320f6dfd9a9421fafa10c1fc8eea122f4d
Ly Xuan Sang <lyxuansang1991@gmail.com>
2017-06-22 15:22:36 +0700

    add data grid admin portal

---
+++ src/components/loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './loading.css';
+
+const Loading = () => (
+    <div className="loading-shading">
+        <span className="glyphicon glyphicon-refresh loading-icon"/>
+    </div>
+)
+
+export default Loading
--- src/containers/PageContentWrapper.js
+++ src/containers/PageContentWrapper.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PageContentHeader from './PageContentHeader';
 import {renderRoutes} from 'react-router-config'
 import PageContentMain from './PageContentMain'
+import DataGridDemo from '../components/DataGridDemo'
 
 class PageContainerWrapper extends Component {
     render() {
@@ -10,8 +11,8 @@ class PageContainerWrapper extends Component {
                 <div className="page-content">
                     {/*BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM*/}
                     {console.log('routes:', this.props)}
-                    <PageContentHeader content_title={this.props.content_title}/>
-                    <PageContentMain/>
+                    <PageContentHeader content_title={this.props.content_title}/> {/*<PageContentMain/>*/}
+                    <DataGridDemo/>
                 </div>
             </div>
         )
